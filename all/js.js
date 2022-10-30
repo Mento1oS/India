cc52f1056fffc41ca9f8348f3d054741523208a1
Mento1oS <103687418+Mento1oS@users.noreply.github.com>
2022-10-30 11:51:55 +0300

    Add files via upload

---
+++ all/js.js
@@ -0,0 +1,38 @@
+let body = document.querySelector('.body');
+let trick = document.querySelector('.trick');
+let pat1 = document.querySelector('.pattern_1');
+let pat2 = document.querySelector('.pattern_2');
+let pat3 = document.querySelector('.pattern_3');
+let pat4 = document.querySelector('.pattern_4');
+let a = 0;
+pat1.addEventListener('click', () => {
+    pat1.style.transition = "all 0.5s ease-in";
+    pat1.style.transform = "translateX(-100vw)";
+    a += 1;
+    check();
+});
+pat2.addEventListener('click', () => {
+    pat2.style.transition = "all 0.5s ease-in";
+    pat2.style.transform = "translateX(100vw)";
+    a += 1;
+    check();
+});
+pat3.addEventListener('click', () => {
+    pat3.style.transition = "all 0.5s ease-in";
+    pat3.style.transform = "translateX(-100vw)";
+    a += 1;
+    check();
+});
+pat4.addEventListener('click', () => {
+    pat4.style.transition = "all 0.5s ease-in";
+    pat4.style.transform = "translateX(100vw)";
+    a += 1;
+    check();
+});
+let check = () => {
+    if (a === 4) {
+        body.classList.remove('none');
+        trick.style.display = 'none';
+        window.top.document.title = "бар ма?";
+    }
+}
